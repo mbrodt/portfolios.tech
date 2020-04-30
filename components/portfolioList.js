db4a2e1ba112789f668436a61b22dccda146ec71
mbrodt <madsbrodt3@gmail.com>
2020-04-30 18:33:34 +0200

    rework portfolio cards structure and adjustments for mobile

--- components/portfolioList.js
+++ components/portfolioList.js
@@ -1,7 +1,7 @@
 const PortfolioList = ({ portfolios, handleTagClick }) => {
   return (
-    <div className="w-full max-w-7xl mx-auto mt-8">
-      <div className="grid grid-cols-3 gap-8">
+    <div className="w-full max-w-8xl mx-auto mt-8 px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 row-gap-8 col-gap-4 md:gap-8 xl:gap-12">
         {portfolios.map((portfolio) => {
           const {
             id,
@@ -12,13 +12,14 @@ const PortfolioList = ({ portfolios, handleTagClick }) => {
             portfolio_tags: tags,
           } = portfolio;
           return (
-            <div key={id} className="rounded">
-              <div className="relative flex justify-center items-center h-64 group">
-                <img
-                  className="absolute h-full group-hover:opacity-25 transition-opacity ease-in duration-150"
-                  src={image}
-                  alt=""
-                />
+            <div key={id} className="rounded bg-white shadow-2xl">
+              <div className="relative flex justify-center items-center h-48 lg:h-64 group bg-cover">
+                <div
+                  className="w-full h-full absolute group-hover:opacity-25 transition-opacity ease-in duration-150"
+                  style={{
+                    backgroundImage: `url(${image})`,
+                  }}
+                ></div>
                 <div className="relative z-10 opacity-0 group-hover:opacity-100 text-center p-4 transition-opacity ease-in duration-150">
                   <p className="text-gray-800 text-xl font-bold">
                     {description}
@@ -32,16 +33,33 @@ const PortfolioList = ({ portfolios, handleTagClick }) => {
                   </a>
                 </div>
               </div>
-              <div className="p-2">
-                <h4 className="text-gray-800 font-bold text-2xl">{title}</h4>
+              <div className="px-4 pt-4 pb-6">
+                <a
+                  href={link}
+                  target="_blank"
+                  className="text-gray-800 font-bold text-2xl"
+                >
+                  {title}{" "}
+                  <svg
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    className="w-4 h-4 inline text-gray-500 "
+                  >
+                    <path d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+                  </svg>
+                </a>
 
-                <ul>
+                <ul className="mt-1">
                   {tags.map((tag) => (
                     <li
                       onClick={() => handleTagClick(tag.tag_value)}
                       key={tag.tag_value}
                       className="bg-indigo-200 text-indigo-700 text-xs font-bold uppercase inline-block
-										leading-4 px-2 py-1 mx-1 text-white mt-2 rounded"
+										leading-4 px-2 py-1 mr-1 mt-1 text-white rounded"
                     >
                       {tag.tag_value}
                     </li>
